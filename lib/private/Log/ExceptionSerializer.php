cba0da26491e0a1dd34e425e130124ecde61ca17
Ferdinand Thiessen <opensource@fthiessen.de>
2024-09-24 23:37:18 +0200

    refactor(encryption): Migrate away from Hooks to typed events
    Co-authored-by: Ferdinand Thiessen <opensource@fthiessen.de>
    Co-authored-by: Louis <louis@chmn.me>
    Co-authored-by: Côme Chilliet <91878298+come-nc@users.noreply.github.com>
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/private/Log/ExceptionSerializer.php
+++ lib/private/Log/ExceptionSerializer.php
@@ -13,8 +13,9 @@ use OCA\Encryption\Controller\RecoveryController;
 use OCA\Encryption\Controller\SettingsController;
 use OCA\Encryption\Crypto\Crypt;
 use OCA\Encryption\Crypto\Encryption;
-use OCA\Encryption\Hooks\UserHooks;
 use OCA\Encryption\KeyManager;
+use OCA\Encryption\Listeners\UserEventsListener;
+use OCA\Encryption\Services\PassphraseService;
 use OCA\Encryption\Session;
 use OCP\HintException;
 
@@ -152,14 +153,16 @@ class ExceptionSerializer {
 		\OCA\Encryption\Users\Setup::class => [
 			'setupUser',
 		],
-		UserHooks::class => [
-			'login',
-			'postCreateUser',
-			'postDeleteUser',
-			'prePasswordReset',
-			'postPasswordReset',
-			'preSetPassphrase',
-			'setPassphrase',
+		UserEventsListener::class => [
+			'handle',
+			'onUserCreated',
+			'onUserLogin',
+			'onBeforePasswordUpdated',
+			'onPasswordUpdated',
+			'onPasswordReset',
+		],
+		PassphraseService::class => [
+			'setPassphraseForUser',
 		],
 	];
 
